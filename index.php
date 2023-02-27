560da65536bbd1096a60c959cf1ddcef85b10eab
iKamuui <99209311+iKamuui@users.noreply.github.com>
2023-02-27 16:45:39 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=<device-width>, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <h1>
+        <?php 
+        echo ( 'Ola mundo')
+        ?>
+    </h1>
+    <h2>
+    <?php
+
+    echo (5 % 3)."\n";          
+    echo (5 % -3)."\n";          
+    echo (-5 % 3)."\n";         
+    echo (-5 % -3)."\n";         
+
+?>
+    </h2> 
+</body>
+</html>
